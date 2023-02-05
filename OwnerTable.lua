ae6cd9799c09857691352f5755e932acd5a38c89
EXTb <100034435+EXFTB@users.noreply.github.com>
2023-02-05 10:45:31 +0100

    Update OwnerTable.lua

--- OwnerTable.lua
+++ OwnerTable.lua
@@ -1,9 +1,8 @@
 return {
-    [355416854] = true;
-    [2751780077] = true;
-    [1314453163] = true;
-    [1799330773] = true;
-    [1724416691] = true;
-    [954005244] = true;
-    [340544469] = true;
+    [AspectedRatio] = true;
+    [UrSoBadOutOfMyGame] = true;
+    [AntiTrxHard] = true;
+    [testttttttttt7] = true;
+    [rotikaru] = true;
+    [vhqals] = true;
 }
